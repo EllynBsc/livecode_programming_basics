eb576fad4f372c728a37ec57275ad9246314a3f3
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2020-05-05 18:18:54 +0200

    livecode

--- price_is_right.rb
+++ price_is_right.rb
@@ -1,5 +1,32 @@
-computer_guess = rand(1..10)
 
+# first version with Hassan
+# computer_guess = rand(1..10)
+# puts 'What is your guess youn human?'
+# user_guess = gets.chomp.to_i
+
+# if computer_guess != user_guess
+#    puts "nope try again"
+# else
+#    puts "you win"
+# end
+
+
+
+# Second version with Hassan with a while loop
+# computer_guess = rand(1..10)
+# puts 'What is your guess youn human?'
+# user_guess = gets.chomp.to_i
+
+# while computer_guess != user_guess
+#   puts "nope try again"
+#   user_guess = gets.chomp.to_i
+# end
+
+# puts "you win thank you bye, your guess was #{user_guess} and computer guess was #{computer_guess}"
+
+# # Third version with refactoring
+
+computer_guess = rand(1..10)
 user_guess = nil
 
 while computer_guess != user_guess
